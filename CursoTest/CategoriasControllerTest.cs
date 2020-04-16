164f2218a3f932de807867c94116d3b4f1e2915a
askrj <33630041+askrj@users.noreply.github.com>
2020-04-16 10:27:38 -0300

    Initial commit

---
+++ CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+    {
+        public CategoriasControllerTest()
+        {
+
+        }
+    }
+}
